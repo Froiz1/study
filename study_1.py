4af6f7942ffe82b62283af71980b15c4305df381
froiz_ <ltvn.daniil@gmail.com>
2024-12-06 09:17:48 -0800

    A program to determine the day of the week by its number.

--- study_1.py
+++ study_1.py
@@ -1,34 +1,16 @@
-print()
-user_1 = {
-    "name": "admin",
-    "password": "password123",
-    "age": 26,
-    "status": True
-}
-user_2 = {
-    "name": "user",
-    "password": "hello123",
-    "age": 17,
-    "status": True
-}
-user_3 = {
-    "name": "player",
-    "password": "player123",
-    "age": 33,
-    "status": False
+week_days = {
+    1: "Sunday",
+    2: "Monday",
+    3: "Tuesday",
+    4: "Wednesday",
+    5: "Thursday",
+    6: "Friday",
+    7: "Saturday",
 }
 
-user_name_list = ["admin", "user", "player"]
-user_password_list = ["password123", "hello123", "player123"]
+user_day = 5
 
-if user_1["status"]:
-    if not user_1["name"] in user_name_list:
-        print("Неправильне ім'я")
-    elif not user_1["password"] in user_password_list:
-        print("Неправильний пароль")
-    elif user_1["age"] < 18:
-        print("Тобі має бути 18 років")
-    else:
-        print(f"Вітаю вас {user_1["name"]} !!!")
-else:
+if user_day < 1 or user_day > 7:
     print("Error")
+else:
+    print(week_days[user_day])
