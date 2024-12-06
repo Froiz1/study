1ec942dc748afe1f62fb15d0cf7447e76c206ae3
froiz_ <ltvn.daniil@gmail.com>
2024-12-06 08:39:49 -0800

    A program to verify login and password.

--- study_1.py
+++ study_1.py
@@ -1,9 +1,34 @@
-user_num = 5
+print()
+user_1 = {
+    "name": "admin",
+    "password": "password123",
+    "age": 26,
+    "status": True
+}
+user_2 = {
+    "name": "user",
+    "password": "hello123",
+    "age": 17,
+    "status": True
+}
+user_3 = {
+    "name": "player",
+    "password": "player123",
+    "age": 33,
+    "status": False
+}
 
-if user_num < 0:
-    print("Error")
+user_name_list = ["admin", "user", "player"]
+user_password_list = ["password123", "hello123", "player123"]
+
+if user_1["status"]:
+    if not user_1["name"] in user_name_list:
+        print("Неправильне ім'я")
+    elif not user_1["password"] in user_password_list:
+        print("Неправильний пароль")
+    elif user_1["age"] < 18:
+        print("Тобі має бути 18 років")
+    else:
+        print(f"Вітаю вас {user_1["name"]} !!!")
 else:
-    factorial = 1
-    for i in range(1, user_num + 1):
-        factorial *= i
-    print(f"Your factorial num for {user_num} = {factorial}")
+    print("Error")
