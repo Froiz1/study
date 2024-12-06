ab90af3659345629b0e6de65c613a1d3a68b7c05
froiz_ <ltvn.daniil@gmail.com>
2024-12-05 20:57:43 -0800

    A program for displaying a list of numbers and calculating their sum.

--- study_1.py
+++ study_1.py
@@ -1,7 +1,3 @@
-for a in range(2, 50 + 1):
-    count = 0
-    for b in range(1, a + 1):
-        if a % b == 0 and a != b and b != 1:
-         count += 1
-    if count == 0:
-        print(a)
+nums = [1, 2, 3, 4, 5, 6, 7]
+
+print(nums, sum(nums))
