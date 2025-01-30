62b2344738eb60d9e3213b346c7d7c8e9177ed8c
froiz_ <ltvn.daniil@gmail.com>
2025-01-29 17:15:30 -0800

    Explored creating simple functions using the lambda command.

--- study_1.py
+++ study_1.py
@@ -1,18 +1,4 @@
-def minimal(l):
-    min_number = l[0]
-    for el in l:
-        if el < min_number:
-         min_number = el
+data = lambda x, y: x * y
+res = data(10, 2)
 
-    return min_number
-
-nums1 = [5, 7, 2, 9, 4, 1]
-min1 = minimal(nums1)
-
-nums2 = [5.4, 7.2, 2.3, 2.1, 9.4, 4.21]
-min2 = minimal(nums2)
-
-if min1 < min2:
-    print(min1)
-else:
-    print(min2)
+print(res)
